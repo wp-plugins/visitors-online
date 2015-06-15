ed6b82555f63792667b2330f3caa40ef697ffeb1
bestwebsoft <bestwebsoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-15 07:27:14 +0000

    V0.1 - The code refactoring was performed. Added detection of the country
    git-svn-id: https://plugins.svn.wordpress.org/visitors-online/trunk@1180911 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ import-country.php
@@ -0,0 +1,218 @@
+<?php
+/* Count the number of lines in the file GeoIPCountryWhois.csv */
+if ( ! function_exists( 'vstrsnln_count_rows' ) ) {
+    function vstrsnln_count_rows( $noscript = false ) {
+        if ( false == $noscript )
+            check_ajax_referer( 'bws_plugin', 'vstrsnln_ajax_nonce_field' );
+
+        $handle = fopen( plugin_dir_path( __FILE__ ) . 'GeoIPCountryWhois.csv', 'r' );
+        if ( $handle ) {
+            $file_number = 1;
+            /* On how many lines in the file */
+            $vstrsnln_lines_count = ( $noscript == false ) ? 3000 : 1000;
+            if ( ! is_writable( plugin_dir_path( __FILE__ ) ) ) {
+                return 0;
+                exit;
+            }
+            $current_file = fopen( plugin_dir_path( __FILE__ ) . 'file_' . $file_number . '.csv', 'a' );
+            if ( $current_file ) {
+                $i = 0;
+                while ( !feof( $handle ) ) {
+                    $line = fgets( $handle );
+                    fwrite( $current_file, $line );
+                    $i++;
+                    if ( $i == $vstrsnln_lines_count ) {
+                        fclose( $current_file );
+                        $file_number++;
+                        $current_file = fopen( plugin_dir_path( __FILE__ ) . 'file_' . $file_number . '.csv', 'a' );
+                        if ( $current_file ) {
+                            $i = 0;
+                        } else {
+                            if ( $noscript == false ) {
+                                echo 0;
+                            } else {
+                                return 0;
+                            }
+                        }                                            
+                    }
+                }
+                fclose( $current_file );
+                fclose( $handle );
+                if ( $noscript == false ) {
+                    echo $file_number;
+                } else {
+                    return $file_number;
+                }
+            } else {
+                if ( $noscript == false ) {
+                    echo 0;
+                } else {
+                    return 0;
+                }
+            }                
+        } else {
+            if ( $noscript == false ) {
+                echo 0;
+            } else {
+                return 0;
+            }
+        }
+        /* This is required to terminate immediately and return a proper response */
+        wp_die();
+    }
+}
+
+/* Fill in the table of country */
+if ( ! function_exists( 'vstrsnln_insert_rows' ) ) {
+    function vstrsnln_insert_rows( $number_file = false, $noscript = false ) {
+        global $wpdb, $wp_filesystem;
+        if ( false == $noscript )
+            check_ajax_referer( 'bws_plugin', 'vstrsnln_ajax_nonce_field' );
+        $prefix_bws             = $wpdb->base_prefix . 'bws_';
+        $vstrsnln_access_type = get_filesystem_method();
+        if ( $vstrsnln_access_type == 'direct' ) {
+            $vstrsnln_creds = request_filesystem_credentials( site_url() . '/wp-admin/', '', false, false, array() );
+            if ( ! WP_Filesystem( $vstrsnln_creds ) ) {
+                if ( $number_file == false ) {
+                    echo false;
+                } else {
+                    return false;
+                }
+            }
+            if ( $number_file == false ) {
+                if ( isset( $_POST['count'] ) && file_exists( plugin_dir_path( __FILE__ ) . 'file_' . $_POST['count'] . '.csv' ) ) {
+                    $filename = plugin_dir_path( __FILE__ ) . 'file_' . $_POST['count'] . '.csv';
+                    $data_array = $wp_filesystem->get_contents_array( $filename );
+                    if ( false !== $data_array && is_array( $data_array ) && ! empty( $data_array ) ) {
+                        $sql = "INSERT IGNORE INTO `" . $prefix_bws . "country`
+                            ( `ip_from`, `ip_to`, `ip_from_int`, `ip_to_int`, `short_country`, `name_country` )
+                            VALUES ( " . implode( " ) , ( ", $data_array ) . " );";
+                        $result = $wpdb->query( $sql );
+                        unlink( $filename );
+                        echo $result;
+                    }
+                }
+            } else {
+                if ( $number_file > 0 && file_exists( plugin_dir_path( __FILE__ ) . 'file_' . $number_file . '.csv' ) ) {
+                    $filename   = plugin_dir_path( __FILE__ ) . 'file_' . $number_file . '.csv';
+                    $data_array = $wp_filesystem->get_contents_array( $filename );
+                    if ( false !== $data_array && is_array( $data_array ) && ! empty( $data_array ) ) {
+                        $sql = "INSERT IGNORE INTO `" . $prefix_bws . "country`
+                            ( `ip_from`, `ip_to`, `ip_from_int`, `ip_to_int`, `short_country`, `name_country` )
+                            VALUES ( " . implode( " ) , ( ", $data_array ) . " );";
+                        $result = $wpdb->query( $sql );
+                        unlink( $filename );
+                        return $result;
+                    }
+                }
+            }
+        }
+        /* This is required to terminate immediately and return a proper response */
+        wp_die();
+    }
+}
+
+/* Importing countries with javascript disabled */
+if ( ! function_exists( 'vstrsnln_import_noscript' ) ) {
+    function vstrsnln_import_noscript( $count_files ) {
+        for ( $count = 1; $count <= $count_files ; $count++ ) {
+            $result = vstrsnln_insert_rows( $count, true );
+        }
+        if ( 0 == $result )
+            $result = true;
+        return $result;
+    }
+}
+
+/* The conclusion to the settings page of information on imports of table */
+if ( ! function_exists( 'vstrsnln_form_import_country' ) ) {
+    function vstrsnln_form_import_country( $page_url ) {
+        global $wpdb;
+        $prefix_bws = $wpdb->base_prefix . 'bws_';
+        $vstrsnln_table_name = $prefix_bws . 'country';
+        /* Table exists */
+        if ( $wpdb->get_var( "SHOW TABLES LIKE '" . $prefix_bws . "country'" ) == $vstrsnln_table_name ) {
+            $vstrsnln_table_full = $wpdb->get_var( "
+                SELECT count( * )
+                FROM `" . $prefix_bws . "country`
+                LIMIT 1"
+            );
+        } else {
+            $vstrsnln_table_full = 0;
+        }?>
+        <form id="vstrsnln_import_block" method="post" action="<?php echo $page_url; ?>">
+            <div class="vstrsnln-info">
+                <?php $vstrsnln_file_there = 1;
+                if ( file_exists( plugin_dir_path( __FILE__ ) . 'GeoIPCountryWhois.csv' ) ) {
+                    /* Open the file in read mode */
+                    $current_file = fopen( plugin_dir_path( __FILE__ ) . 'GeoIPCountryWhois.csv', 'r' ); 
+                    if ( $current_file ) {
+                        _e( 'Importing country information from the GeoIPCountryWhois.csv file', 'visitors-online' );
+                    } else {
+                        $vstrsnln_content = __( 'You do not have permission to access the file', 'visitors-online' ) . '&#032;&#032;' . plugin_dir_path( __FILE__ ) . 'GeoIPCountryWhois.csv' .
+                        '&#044;&#032;&#032;' . __( 'cannot be imported', 'visitors-online' );
+                        echo $vstrsnln_content;
+                        $vstrsnln_file_there = 0;
+                    }
+                } else {
+                    $vstrsnln_content = plugin_dir_path( __FILE__ ) . 'GeoIPCountryWhois.csv' . '&#032;&#032;' . __( 'the file is not found, import is impossible', 'visitors-online' );
+                    echo $vstrsnln_content;
+                    $vstrsnln_file_there = 0;
+                } ?>
+                <div class="vstrsnln_clear"></div>
+                <?php if ( $vstrsnln_table_full > 0 ) {
+                    _e( 'The table is already loaded, you can also update the data using this instructions', 'visitors-online' );
+                } else {
+                    _e( 'Instructions on downloading and updating the country table', 'visitors-online' );
+                } ?>
+                 <a href="https://docs.google.com/document/d/1sxxeDleJdPS8HvRdYwYSABQ586t1s-Z8r6wy55iXJCM/edit" target="_blank">https://docs.google.com/document/d/1sxxeDleJdPS8HvRdYwYSABQ586t1s-Z8r6wy55iXJCM/edit</a>
+            </div>
+            <div class="vstrsnln_clear"></div>
+            <?php if ( 1 == $vstrsnln_file_there ) {
+                if ( $vstrsnln_table_full > 0 ) { ?>
+                    <input id="vstrsnln_button_import" type="submit" name="vstrsnln_button_import" class="button-primary" value="<?php _e( 'Update', 'visitors-online' ) ?>" />
+                <?php } else {?>
+                    <input id="vstrsnln_button_import" type="submit" name="vstrsnln_button_import" class="button-primary" value="<?php _e( 'Import', 'visitors-online' ) ?>" />
+                <?php }
+            } ?>
+            <?php wp_nonce_field( plugin_basename( __FILE__ ), 'vstrsnln_nonce_name' ); ?>
+            <input type="hidden" name="vstrsnln_import" value="submit" />
+            <div id="vstrsnln_img_loader">
+                <img src="<?php echo plugins_url( 'images/ajax-loader.gif', __FILE__ ); ?>" alt="" />
+            </div>
+            <div id="vstrsnln_message">
+                <?php echo __( 'Number of loaded files', 'visitors-online' ) . '&#058;'; ?> <span id='vstrsnln_loaded_rows'></span>
+                <?php echo __( 'Number of a loading file', 'visitors-online' ) . '&#058;'; ?> <span id='vstrsnln_loaded_files'></span>
+            </div>
+        </form>
+    <?php }
+}
+
+/* Pressing the 'Import Country' */
+if ( ! function_exists( 'vstrsnln_press_buttom_import' ) ) {
+    function vstrsnln_press_buttom_import() {
+        $message = $error = '';
+        if ( isset( $_REQUEST['vstrsnln_button_import'] ) && check_admin_referer( plugin_basename( __FILE__ ), 'vstrsnln_nonce_name' ) ) {
+            $vstrsnln_count_files = vstrsnln_count_rows( true );
+            if ( $vstrsnln_count_files == 0 ) {
+                $error = __( 'Not enough rights to import from the GeoIPCountryWhois.csv file, import is impossible', 'visitors-online' );
+                $result = false;
+            } else {
+                $vstrsnln_result = vstrsnln_import_noscript( $vstrsnln_count_files );
+                if ( $vstrsnln_result == true ) {
+                    $message = __( 'Import was finished.', 'visitors-online' );
+                    $result = true;
+                } else {
+                    $error = __( 'Not enough rights to import from the GeoIPCountryWhois.csv file, import is impossible', 'visitors-online' );
+                    $result = false;
+                }
+            }
+        } else {
+            $result = 0;
+        }
+        return array( 
+            'result'    => $result, 
+            'error'     => $error,
+            'message'   => $message);        
+    }
+} ?>
---
+++ readme.txt
@@ -0,0 +1,105 @@
+=== Visitors online by BestWebSoft ===
+Contributors: bestwebsoft
+Donate link: http://bestwebsoft.com/donate/
+Tags: add User Online plugin, visitors online plugin, UserOnline, UsersOnline, visitors online, count visitors, guests, guestscount, guestsonline, guests online, bots, gests, online guests plugin, a bot, user online, users online, who online, online, users, user, widget, VisitorOnline, Visitors online
+Requires at least: 3.4
+Tested up to: 4.2.2
+Stable tag: 0.1
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Plugin allows to see how many users, guests and bots are online on the website. Also, you can see statistics on the highest number of visits.
+
+== Description ==
+
+This plugin allows you to see the number of visitors on the site. You can set the time when a visitor is considered being online on the plugin settings page. The plugin provides the ability to display statistics on countries and browsers. All visitors are identified as registered users, guests or bots. You can also see the day with the highest number of visits. Information about visitors can be displayed using a widget or shortcode. Also, it can be seen on the Dashboard.
+
+<a href="http://wordpress.org/plugins/visitors-online/faq/" target="_blank">FAQ</a>
+
+<a href="http://support.bestwebsoft.com" target="_blank">Support</a>
+
+= Features =
+
+* Allows to set the time when the visitor is considered being online without making any actions
+* Allows you to set the details on countries and browsers
+* Allows to clear statistics
+* Displays information on the Dashboard
+* Allows you to display information using a widget or shortcode
+
+= How It Works = 
+
+- the user will be displayed online, if he/she had left the site, but the time when the user is considered being online is not yet passed. 
+- to define the user`s country, you will need to download the file according to the instruction https://docs.google.com/document/d/1sxxeDleJdPS8HvRdYwYSABQ586t1s-Z8r6wy55iXJCM/edit
+- if the number of visits from different countries is the same, the plugin will display several countries, but not more than three;
+- if the number of visits from different browsers is the same, the plugin will display several browsers, but not more than three
+
+= Translation =
+
+* Russian (ru_RU)
+* Ukrainian (uk)
+
+If you would like to create your own language pack or update the existing one, you can send <a href="http://codex.wordpress.org/Translating_WordPress" target="_blank">the text of PO and MO files</a> to <a href="http://support.bestwebsoft.com" target="_blank">BestWebSoft</a>, and we'll add it to the plugin. You can download the latest version of the program for working with PO and MO files <a href="http://www.poedit.net/download.php" target="_blank">Poedit</a>.
+
+= Technical support =
+
+Dear users, our plugins are available for free download. If you have any questions or recommendations regarding the functionality of our plugins (existing options, new options, current issues), please feel free to contact us. Please note that we accept requests in English only. All messages in another languages won't be accepted.
+
+If you notice any bugs in the plugin's work, you can notify us about them and we'll investigate and fix the issue then. Your request should contain website URL, issues description and WordPress admin panel credentials.
+Moreover, we can customize the plugin according to your requirements. It's a paid service (as a rule it costs $40, but the price can vary depending on the amount of the necessary changes and their complexity). Please note that we could also include this or that feature (developed for you) in the next release and share with the other users then.
+We can fix some things for free for the users who provide a translation of our plugin into their native language (this should be a new translation of a certain plugin, you can check available translations on the official plugin page).
+
+== Installation == 
+
+1. Upload `visitors-online` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin using the 'Plugins' menu in your WordPress admin panel.
+3. You can adjust the necessary settings using your WordPress admin panel in "BWS Plugins" > "Visitors Online".
+4. Create a page or a post and insert the short-code [vstrsnln_info] into the text.
+5. Add a widget Visitors Online to the Sidebar column.
+
+== Frequently Asked Questions ==
+
+= I get "Not enough rights to import from the GeoIPCountryWhois.csv file" error. What shall I do? =
+
+You should set rights 755 to the folders wp-content, plugins, visitors-online and 644 to the GeoIPCountryWhois.csv file. In such case the plugin’s script will have enough rights to upload the file. Here is some useful information for you <a href="http://codex.wordpress.org/Changing_File_Permissions#Shared_Hosting_with_suexec" target="_blank">http://codex.wordpress.org/Changing_File_Permissions#Shared_Hosting_with_suexec</a>.
+
+= Where can I find statistics? =
+
+You can see statistics on the admin panel, in any place of a post (if using shortcode), or in a widget (if this widget is added to the sidebar).
+
+= Why do the number of users online displayed in statistics is greater, than it actually is? =
+
+The user will be displayed online, if he/she had left the site, but the time when the user is considered being online is not yet passed.
+
+= How bots are defined? =
+
+The plugin receives data from the server variable $_SERVER['HTTP_USER_AGENT'], and searches the resulting value in its list of the most common bots. Once a match was found, the visitor is considered being a bot.
+
+= Why the information about the day with the maximum number of visits does not match the number of actual visitors? =
+
+The plugin counts the number of the site visitors during the day. If the same user visits the site 5 times during the day, the plugin see 5 visits, but not one. The visit means that a user enters the site or console as a guest user or bot, and stays within the time set on the plugin settings page. (The time period when the user is online, without making any actions).
+
+= I have some problems with the plugin's work. What Information should I provide to receive proper support? =
+
+Please make sure that the problem hasn't been discussed on our forum yet (<a href="http://support.bestwebsoft.com" target="_blank">http://support.bestwebsoft.com</a>). If not, please provide the following data along with the description of your problem:
+
+1. the link to the page, on which the problem occurs
+2. the name of the plugin and its version. If you are using a pro version - your order number.
+3. the version of your WordPress installation
+4. your system status report. Please read more here: <a href="https://docs.google.com/document/d/1Wi2X8RdRGXk9kMszQy1xItJrpN0ncXgioH935MaBKtc/edit" target="_blank">Instuction on System Status</a> 
+
+== Screenshots ==
+
+1. Plugin Settings page.
+2. Displaying Visitors online on the Dashboard.
+3. Displaying Visitors online in the Sidebar on your WordPress website.
+
+== Changelog ==
+
+= V0.1 - 15.06.2015 = 
+* Bugfix : The code refactoring was performed.
+* NEW : Added detection of the country.
+
+== Upgrade Notice ==
+
+= V0.1 =
+The code refactoring was performed. Added detection of the country
